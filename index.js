763c5632495518e8b58ae58531f012bccdb598a5
Rayees ahmad rather <rayeesrather35@gmail.com>
2023-06-06 22:46:04 +0530

    add initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const darkModeToggle = document.getElementById("dark-mode-toggle");
+const body = document.body;
+const heading = document.getElementById("heading");
+
+darkModeToggle.addEventListener("change", () => {
+    body.classList.toggle("dark-mode");
+    if (darkModeToggle.checked) {
+        heading.style.color = "#fff";
+    } else {
+        heading.style.color = "#D2001A";
+    }
+});
+
+document.querySelector(".add").addEventListener("click", () => {
+    const inputTxt = document.querySelector(".input");
+    const addList = document.querySelector(".addList");
+
+    if (inputTxt.value === "") {
+        return;
+    }
+
+    const li = document.createElement('li');
+    li.innerHTML = `<span>${inputTxt.value}</span><button class="delete">x</button>`;
+    addList.appendChild(li);
+    inputTxt.value = "";
+
+    const deleteButtons = li.querySelectorAll(".delete");
+    deleteButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            button.parentNode.remove();
+        });
+    });
+});
+
+const deleteButtons = document.querySelectorAll(".delete");
+deleteButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        button.parentNode.remove();
+    });
+});
