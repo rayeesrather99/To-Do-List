f54176a1c4237d8031f5f796eb42203a674d912f
Rayees ahmad rather <rayeesrather35@gmail.com>
2023-07-02 16:16:38 +0530

    removed the bug from search

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const darkModeToggle = document.getElementById("dark-mode-toggle");
 const body = document.body;
 const heading = document.getElementById("heading");
 
-//dark/light mode 
 darkModeToggle.addEventListener("change", () => {
     body.classList.toggle("dark-mode");
     if (darkModeToggle.checked) {
@@ -12,7 +11,6 @@ darkModeToggle.addEventListener("change", () => {
     }
 });
 
-//adding task 
 document.querySelector(".add").addEventListener("click", () => {
     const inputTxt = document.querySelector(".input");
     const addList = document.querySelector(".addList");
@@ -34,33 +32,38 @@ document.querySelector(".add").addEventListener("click", () => {
     });
 });
 
-//delete task
+// remove task 
 const deleteButtons = document.querySelectorAll(".delete");
 deleteButtons.forEach(button => {
     button.addEventListener("click", () => {
-        button.parentNode.remove();
+        if(confirm("Are you sure!")){
+            button.parentNode.remove();
+        }
+        
     });
 });
 
+
 //search button 
     
-const searchInput = document.querySelector('.search-input');
-searchInput.addEventListener('keyup', filterTasks);
+    const searchInput = document.querySelector('.search-input');
+    searchInput.addEventListener('keyup', filterTasks);
 
-function filterTasks() {
-    const searchTerm = searchInput.value.toLowerCase();
+    function filterTasks() {
+        const searchTerm = searchInput.value.toLowerCase();
 
-    // all the list items
-    const listItems = document.querySelectorAll('.addList li');
+        // all the list items
+        const listItems = document.querySelectorAll('.addList li');
 
-    // Iterate over each item in the list
-    listItems.forEach((item) => {
-        const taskName = item.querySelector('span').textContent.toLowerCase();
+        // Iterate over each item in the list
+        listItems.forEach((item) => {
+            const taskName = item.querySelector('span').textContent.toLowerCase();
 
-        // taskName.includes(searchTerm) -->[ here it means ,is the taskname that appeared in search input is in the list of items if yes then it will displayed otherwise none]
-        if (taskName.includes(searchTerm)) {
-            item.style.display = 'flex'; // Show the list item
-        } else {
-            item.style.display = 'none'; // Hide the list item
-        }
-    });
+            // taskName.includes(searchTerm) -->[ here it means ,is the taskname that appeared in search input is in the list of items if yes then it will displayed otherwise none]
+            if (taskName.includes(searchTerm)) {
+                item.style.display = 'flex'; // Show the list item
+            } else {
+                item.style.display = 'none'; // Hide the list item
+            }
+        });
+    }
